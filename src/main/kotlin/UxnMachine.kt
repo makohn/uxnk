8240f670e60810b8f3b229810e52de5cc8285579
Marek Kohn <me@makohn.de>
2023-11-12 21:27:36 +0100

    Implement ALU instructions

--- src/main/kotlin/UxnMachine.kt
+++ src/main/kotlin/UxnMachine.kt
@@ -22,22 +22,6 @@ data class UxnMachine(
                 return MachineState.Stopped
             }
 
-            OpCode.INC -> {
-                workingStack.addLast(workingStack.removeLast().inc())
-            }
-
-            OpCode.POP -> {
-                workingStack.removeLast()
-            }
-
-            OpCode.NIP -> {
-                workingStack.removeAt(workingStack.size - 2)
-            }
-
-            OpCode.SWP -> {
-
-            }
-
             OpCode.LIT -> {
                 workingStack.addLast(memory[pc])
                 pc++
@@ -47,29 +31,93 @@ data class UxnMachine(
                 val device = workingStack.removeLast()
                 print(workingStack.removeLast().toInt().toChar())
             }
-            else -> println("Opcode '$opCode' is not implemented yet!")
+            else -> with(if (opCode.short()) ShortMode() else ByteMode()) {
+                evaluate(opCode, workingStack)
+            }
         }
         return MachineState.Running
     }
 
-    context(StackContext<*>)
-    fun evaluate(opCode: OpCode) {
+    context(OpMode)
+    private fun evaluate(opCode: OpCode, stack: ArrayDeque<UByte>) {
         when (opCode.base()) {
-            else -> Unit
+            OpCode.INC -> stack.push(stack.pop().inc())
+            OpCode.POP -> stack.pop()
+            OpCode.NIP -> {
+                val v = stack.pop()
+                stack.pop()
+                stack.push(v)
+            }
+            OpCode.SWP -> {
+                val (b, a) = stack.pop(2)
+                stack.push(b)
+                stack.push(a)
+            }
+            OpCode.ROT -> {
+                val (c, b, a) = stack.pop(3)
+                stack.push(b)
+                stack.push(c)
+                stack.push(a)
+            }
+            OpCode.DUP -> {
+                val v = stack.pop()
+                stack.push(v)
+                stack.push(v)
+            }
+            OpCode.OVR -> {
+                val (b, a) = stack.pop(2)
+                stack.push(a)
+                stack.push(b)
+                stack.push(a)
+            }
+            OpCode.EQU -> stack.pushBool(stack.pop() == stack.pop())
+            OpCode.NEQ -> stack.pushBool(stack.pop() != stack.pop())
+            OpCode.GTH -> stack.pushBool(stack.pop() > stack.pop())
+            OpCode.LTH -> stack.pushBool(stack.pop() < stack.pop())
+            OpCode.ADD -> stack.push(stack.pop() + stack.pop())
+            OpCode.SUB -> {
+                val (b, a) = stack.pop(2)
+                stack.push(a - b)
+            }
+            OpCode.MUL -> stack.push(stack.pop() * stack.pop())
+            OpCode.DIV -> {
+                val (b, a) = stack.pop(2)
+                require(b != 0.toUShort()) { "error: division by zero!" }
+                stack.push(a / b)
+            }
+            OpCode.AND -> stack.push(stack.pop() and stack.pop())
+            OpCode.ORA -> stack.push(stack.pop() or stack.pop())
+            OpCode.EOR -> stack.push(stack.pop() xor stack.pop())
+            else -> error("error: $opCode not implemented!")
         }
     }
 
-    interface StackContext<E> {
-        fun ArrayDeque<UByte>.push(element: E)
-        fun ArrayDeque<UByte>.pop(): E
+    // As Kotlin does not have union types (and UByte and UShort do not share a common interface for math operations),
+    // we have to coerce into UShort as an upper boundary to allow for executing math operations regardless of the
+    // concrete context used.
+    //
+    // Otherwise, we could have introduced a generic type parameter T where T is either UShort or UByte (union type)
+    interface OpMode {
+        fun ArrayDeque<UByte>.push(element: UShort)
+        fun ArrayDeque<UByte>.pop(): UShort
+
+        fun ArrayDeque<UByte>.pop(n: Int) = buildList {
+            repeat(n) {
+                add(pop())
+            }
+        }
+
+        fun ArrayDeque<UByte>.pushBool(bool : Boolean) = push(if (bool) 1u else 0u)
+
+        fun ArrayDeque<UByte>.push(int: UInt) = push(int.toUShort())
     }
 
-    interface ByteStack: StackContext<UByte> {
-        override fun ArrayDeque<UByte>.push(element: UByte) = addLast(element)
-        override fun ArrayDeque<UByte>.pop() = removeLast()
+    class ByteMode: OpMode {
+        override fun ArrayDeque<UByte>.push(element: UShort) = addLast(element.toUByte())
+        override fun ArrayDeque<UByte>.pop() = removeLast().toUShort()
     }
 
-    interface ShortStack: StackContext<UShort> {
+    class ShortMode: OpMode {
         override fun ArrayDeque<UByte>.push(element: UShort) {
             addLast((element.toUInt() shr 8).toUByte())
             addLast(element.toUByte())
