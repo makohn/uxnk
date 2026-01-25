affb7adbd5f0885a3ab8f51ee309eace1653a1ff
Marek Kohn <me@makohn.de>
2026-01-25 15:34:18 +0100

    Refactor system device

--- src/varvara/System.kt
+++ src/varvara/System.kt
@@ -1,44 +1,31 @@
 package varvara
 
+import util.*
+
 class System : IODevice {
 
-    companion object {
-        const val METADATA: UByte = 0x6u
-        const val RED: UByte = 0x8u
-        const val GREEN: UByte = 0xau
-        const val BLUE: UByte = 0xcu
-    }
+    private val memory = UByteArray(16)
 
-    var metadata: UShort = 0x0u; private set
-    var red: UShort = 0xf07fu; private set
-    var green: UShort = 0xf0d6u; private set
-    var blue: UShort = 0xf0b2u; private set
+    val red: UShort get() = UShort(memory[0x8], memory[0x9])
+    val green: UShort get() = UShort(memory[0xa], memory[0xb])
+    val blue: UShort get() = UShort(memory[0xc], memory[0xd])
 
     override fun write(port: UByte, value: UByte) {
-        TODO("Not yet implemented")
+        memory[port] = value
     }
 
     override fun writeShort(port: UByte, value: UShort) {
-        when (port) {
-            METADATA -> metadata = value
-            RED -> red = value
-            GREEN -> green = value
-            BLUE -> blue = value
-            else -> error("writeShort port=${port.toString(16)}, value=${value.toString(16)}")
-        }
+        write(port, value.hi)
+        write((port + 1u).toUByte(), value.lo)
     }
 
     override fun read(port: UByte): UByte {
-        TODO("Not yet implemented")
+        return memory[port]
     }
 
     override fun readShort(port: UByte): UShort {
-        return when (port) {
-            METADATA -> metadata
-            RED -> red
-            GREEN -> green
-            BLUE -> blue
-            else -> error("readShort port=${port.toString(16)}")
-        }
+        val hi = memory[port]
+        val lo = memory[port + 1u]
+        return UShort(hi, lo)
     }
 }
